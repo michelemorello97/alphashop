db190a8f8306defc174e2ea4041bb18873b53c4c
michelemorello97 <mikimorry2@hotmail.it>
2026-06-03 20:18:41 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('alphashop');
+  saluti: string = 'Benvenuto in alphashop Coglionazzo';
+  bollini: number = 1500;
 }
