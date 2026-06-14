18c9af73d32c3e029619476c585d8c13b3114320
michelemorello97 <mikimorry2@hotmail.it>
2026-06-14 13:48:58 +0200

    Creato il componente grid-articoli per la visualizzazione e filtraggio degli articoli.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { Error } from './pages/error/error';
 import { Articoli } from './pages/articoli/articoli';
 import { Logout } from './pages/logout/logout';
 import { RouteGuard } from '../services/route-guard';
+import { GridArticoli } from './pages/grid-articoli/grid-articoli';
 
 export const routes: Routes = [
     {path: '', component: Login},
     {path: 'login', component: Login},
     {path: 'welcome/:userId', component: Welcome, canActivate: [RouteGuard]},
     {path: 'articoli', component: Articoli, canActivate: [RouteGuard]},
+    {path: 'articoli/grid', component: GridArticoli, canActivate: [RouteGuard]},
     {path: 'logout', component: Logout, canActivate: [RouteGuard]},
     {path: '**', component: Error}
 ];
---
+++ src/app/pages/grid-articoli/grid-articoli.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-articoli',
+  imports: [],
+  templateUrl: './grid-articoli.html',
+  styleUrl: './grid-articoli.css',
+})
+export class GridArticoli {}
